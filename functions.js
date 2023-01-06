2e541d1e8b65554682d2205ea1fc268e3e544110
Melissa Johnson <melissajohnson326@gmail.com>
2023-01-05 23:55:37 -0500

    Add playerSelection variable and prompt for user inpu
    made playerSelection not case sensitive

--- functions.js
+++ functions.js
@@ -9,7 +9,18 @@ function getComputerChoice(){
         case 2 : 
         return 'scissors'
     }
-    
+
 }
 const computerSelection = getComputerChoice()
-console.log (computerSelection)
+console.log (`Computer selects ${computerSelection}`)//prints computer selection
+
+//player selection
+let input = prompt('Choose your weapon')//gives a prompt to player
+let playerSelection =  input.toLowerCase()
+console.log(`Player selects ${playerSelection}.`)//prints player selection
+
+
+//round functions
+function playRound(playerSelection,computerSelection){
+    
+}
