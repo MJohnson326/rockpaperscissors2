0ecd73fa02d654a3f5ab945393b91e71a710a23c
Melissa Johnson <melissajohnson326@gmail.com>
2023-01-06 15:00:58 -0500

    Complete playRound function and allow game to declare a winner

--- functions.js
+++ functions.js
@@ -15,12 +15,73 @@ const computerSelection = getComputerChoice()
 console.log (`Computer selects ${computerSelection}`)//prints computer selection
 
 //player selection
-let input = prompt('Choose your weapon')//gives a prompt to player
-let playerSelection =  input.toLowerCase()
+
+function getPlayerSelection(){
+    let input = prompt('Choose your weapon')//gives a prompt to player
+    let getPlayerSelection =  input.toLowerCase()
+    switch(getPlayerSelection){
+        case 'rock':
+            return 'rock'
+        case 'paper':
+            return 'paper'
+        case 'scissors':
+            return 'scissors'
+        case '':
+            return 'null'
+    }
+
+}
+const playerSelection= getPlayerSelection()
 console.log(`Player selects ${playerSelection}.`)//prints player selection
+
+
+
+//declare game result
+
+let declareWin = `${playerSelection} beats ${computerSelection}. You win!`
+//declares playerSelection is winner
+
+   let declareLose = `${computerSelection} beats ${playerSelection}. You lose!`
+
+//declares player loss
+
+   let declareTie = `${playerSelection} versus ${computerSelection}.  It's a tie!`
+
+//declares tie
+
 
 
 //round functions
 function playRound(playerSelection,computerSelection){
-    
+    while (playerSelection === 'rock'){
+        if (computerSelection ==='rock'){
+            return declareTie
+        } else if (computerSelection ==='paper'){
+            return declareLose
+        } else if(computerSelection ==='scissors'){
+            return declareWin
+        }
+    }
+    while (playerSelection === 'paper'){
+        if(computerSelection ==='rock'){
+            return declareWin
+        } else if (computerSelection ==='paper'){
+            return declareTie
+        } else if (computerSelection === 'scissors'){
+            return declareLose
+        }
+    }''
+    while (playerSelection === 'scissors'){
+        if (computerSelection === 'rock'){
+            return declareLose
+        } else if (computerSelection === 'paper'){
+            return declareWin
+        } else if (computerSelction === 'scissors'){
+            return declareTie
+        }
+    } 
 }
+console.log(playRound(playerSelection,computerSelection))
+
+
+ 
