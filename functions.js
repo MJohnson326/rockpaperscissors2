59016bd7abf82a18d095a95dd4eddd91d69bb805
Melissa Johnson <melissajohnson326@gmail.com>
2023-03-13 01:23:17 -0400

    adjust player click function to get getPlayerSelection variable

--- functions.js
+++ functions.js
@@ -36,65 +36,70 @@ let computerScore = 0;
 function replacingImagePlayerRock() {
     document.getElementById("playerChoice").src = "images/playerrock.png"
     playerChoice.style.visibility = 'visible';
-    getComputerSelection()
-    
+
+
 }
 
 function replacingImagePlayerPaper() {
-    document.getElementById("playerChoice").src ="images/playerpaper.png"
+    document.getElementById("playerChoice").src = "images/playerpaper.png"
     playerChoice.style.visibility = 'visible';
-    getComputerSelection()
+
 }
-   
+
 function replacingImagePlayerScissors() {
     document.getElementById("playerChoice").src = "images/playerscissors.png"
     playerChoice.style.visibility = 'visible';
-    getComputerSelection()
-   
+
+
 }
 
 
 
 
 //randomize computer selection between rock, paper, or scissons
-function getComputerSelection(){
+function getComputerSelection() {
     let getComputerSelection = (Math.floor(Math.random() * 3))  //randomize number 
-        if(getComputerSelection === 0){ //assign random number to computer selection
-            document.getElementById("cpuChoice").src = "images/playerrock.png" //insert rock image 
-            return 'rock'
-        }else if (getComputerSelection === 1 ){//assign random number to computer selection
-            document.getElementById("cpuChoice").src = "images/playerpaper.png"//insert paper image
-            return 'paper'
-        }else if (getComputerSelection === 2){//assign random number to computer selection
-            document.getElementById("cpuChoice").src = "images/playerscissors.png"// insert rock image
-            return 'scissors'
+    if (getComputerSelection === 0) { //assign random number to computer selection
+        document.getElementById("cpuChoice").src = "images/playerrock.png" //insert rock image 
+        return 'rock'
+    } else if (getComputerSelection === 1) {//assign random number to computer selection
+        document.getElementById("cpuChoice").src = "images/playerpaper.png"//insert paper image
+        return 'paper'
+    } else if (getComputerSelection === 2) {//assign random number to computer selection
+        document.getElementById("cpuChoice").src = "images/playerscissors.png"// insert rock image
+        return 'scissors'
 
     }
 }
 let computerSelection = getComputerSelection()
 
-   
-const playerSelection = function(){  
-if (this.id==='rock'){ //run function to input player selection images
-    replacingImagePlayerRock()
-    return 'rock'
-   }else if (this.id=== 'paper'){
-    replacingImagePlayerPaper()
-    return 'paper'
-    }else if (this.id=== 'scissors'){
-        replacingImagePlayerScissors()
-        return 'scissors'
-    } 
-    document.getElementById('cpuChoice') 
+
+document.getElementById('rock').onclick = playerClick
+document.getElementById('paper').onclick = playerClick
+document.getElementById('scissors').onclick = playerClick
+
+function playerClick() {
+    document.getElementById('cpuChoice')
     cpuChoice.style.visibility = "visible" // computer selection visible on click
     getComputerSelection()
-   }
-   
+    let getPlayerSelection = this.id
 
-   document.getElementById('rock').onclick= playerSelection
-   document.getElementById('paper').onclick= playerSelection
-   document.getElementById('scissors').onclick= playerSelection
+    if (getPlayerSelection === 'rock') { //run function to input player selection images
+        replacingImagePlayerRock()
+        return 'rock'
+    } else if (getPlayerSelection === 'paper') {
+        replacingImagePlayerPaper()
+        return 'paper'
+    } else if (getPlayerSelection === 'scissors') {
+        replacingImagePlayerScissors()
+        return 'scissors'
+    }
    
+}
+
+
+
+
 //establish rules/function of rounds
 // function playRound(playerSelection,computerSelection) {
 //     if (playerSelection === computerSelection){
