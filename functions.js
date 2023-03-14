85d2087df4c81eeb1344f354c7d6d81d3bc93e28
Melissa Johnson <melissajohnson326@gmail.com>
2023-03-14 15:00:20 -0400

    fixed js to run game as it should

--- functions.js
+++ functions.js
@@ -1,29 +1,7 @@
-let btns = document.getElementsByClassName('btn') //player choices
 
 let pscore = document.querySelector('.pscore') //player score div
 let cpuscore = document.querySelector('.cpuscore') //computer score div
 
-<<<<<<< HEAD
-//set scores to begin at 0
-let playerScore = 0;
-let computerScore = 0;
-
-//establish rules/function of rounds
-function playRound(playerSelection,computerSelection) {
-    if (playerSelection === computerSelection){
-        return `It\'s a draw! Try again! Score: Player ${playerScore} to computer ${computerScore}`
-    } else if ((playerSelection ==='rock' && computerSelection==='scissors')||
-    (playerSelection ==='paper' && computerSelection ==='rock')||
-    (playerSelection ==='scissors' && computerSelection === 'paper')){
-        playerScore++
-        return `${playerSelection} beats ${computerSelection}. Score: Player ${playerScore} to computer ${computerScore}.`
-    } else if((playerSelection ==='rock' && computerSelection === 'paper')||
-    (playerSelection==='paper' && computerSelection ==='scissors')||
-    (playerSelection === 'scissors' && computerSelection === 'rock')){
-        computerScore++ 
-        return `${computerSelection} beats ${playerSelection}. Score: Player ${playerScore} to computer ${computerScore}.`
-
-=======
 // //set scores to begin at 0
 let playerScore = 0;
 let computerScore = 0;
@@ -37,7 +15,6 @@ function getComputerSelection() {
         return 'paper'
     } else if (getComputerSelection === 2) {//assign random number to computer selection
         return 'scissors'
->>>>>>> rps2-ui
     }
 }
 
@@ -66,16 +43,17 @@ function playRound(playerSelection, computerSelection) {
 
 }
 
-function endGame(){
-    if(playerScore <10 && computerScore<10){
+function endGame() {
+    if (playerScore < 10 && computerScore < 10) {
         console.log('nothing')
-    }else{
-        if((playerScore === 10 || computerScore ===10)&&(playerScore>computerScore)){
-            alert ('You win! Hit refresh to play again!')
-        }else if((playerScore === 10 || computerScore ===10)&&(playerScore<computerScore)){
-            alert ('You lose! Hit refresh to play again!')}
-pscore.innerHTML = 'Your Score: '
-cpuscore.innerHTML = 'Opponent Score: '
+    } else {
+        if ((playerScore === 10 || computerScore === 10) && (playerScore > computerScore)) {
+            alert('You win! Hit refresh to play again!')
+        } else if ((playerScore === 10 || computerScore === 10) && (playerScore < computerScore)) {
+            alert('You lose! Hit refresh to play again!')
+        }
+        pscore.innerHTML = 'Your Score: '
+        cpuscore.innerHTML = 'Opponent Score: '
     }
 }
 
@@ -83,12 +61,12 @@ cpuscore.innerHTML = 'Opponent Score: '
 
 function gamePlay() {
     let btns = document.querySelectorAll('.btn')
-btns.forEach(btn => btn.addEventListener('click', (e) => {
+    btns.forEach(btn => btn.addEventListener('click', (e) => {
         btnPressed = e.target.id;
         let playerSelection = btnPressed
         document.getElementById("playerChoice").src = `images/${playerSelection}.png`
         playerChoice.style.visibility = 'visible';
-        
+
         getComputerSelection()
         let computerSelection = getComputerSelection()
         document.getElementById('cpuChoice').src = `images/${computerSelection}.png`
